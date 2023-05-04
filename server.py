817f58269cf7d66aecb8b45a87579a948053b6ce
Poojan Patel <poojanpatel02112002@gmail.com>
2023-05-04 15:21:29 +0530

    added threaded client server system

--- server.py
+++ server.py
@@ -1,11 +1,19 @@
 import socket
+import threading
+
+def thread_function(c,addr):
+    # while True:
+    print(f"{addr}::{str(c.recv(1024).decode())}")
+    print(f"New: {str(c.recv(1024).decode())}")
+    c.close()
+
 
 s = socket.socket()
 print("Socket created")
 
 s.bind((socket.gethostbyname(socket.gethostname()),8000))
 print(f"IP: {socket.gethostbyname(socket.gethostname())}")
-s.listen(3)
+s.listen(1)
 
 print("Waiting for connection")
 
@@ -13,5 +21,8 @@ while True:
     c,addr = s.accept()
     print("Connection made with ",addr)
     c.send(bytes("Hello connected with me","utf-8"))
-    print(str(c.recv(1024).decode()))
+    # print(str(c.recv(1024).decode()))
+    threading._start_new_thread(thread_function,(c,addr))
+
+s.close()
     
